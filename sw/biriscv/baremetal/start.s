f62396bb99dbe7775edd52de2423a7b7e7c2ed29
Brandon Riggin <eehusky@gmail.com>
2026-05-16 15:01:14 -0400

    Deleted: sw/biriscv/apps/baremetal_demo/CMakeLists.txt sw/biriscv/apps/baremetal_demo/main.c sw/biriscv/apps/freertos_demo/CMakeLists.txt sw/biriscv/apps/freertos_demo/FreeRTOSConfig.h sw/biriscv/apps/freertos_demo/main.c sw/biriscv/apps/ringbuffer_demo/CMakeLists.txt sw/biriscv/apps/ringbuffer_demo/FreeRTOSConfig.h sw/biriscv/apps/ringbuffer_demo/main.c sw/biriscv/baremetal/entry.c sw/biriscv/baremetal/linker.ld sw/biriscv/baremetal/start.s sw/biriscv/biriscv_extensions.h sw/biriscv/freertos/crt0.S sw/biriscv/freertos/linker.ld sw/biriscv/freertos/RISC-V_biriscv/freertos_risc_v_chip_specific_extensions.h sw/biriscv/freertos/RISC-V_biriscv/portASM.S sw/biriscv/freertos/RISC-V_biriscv/port.c sw/biriscv/freertos/RISC-V_biriscv/portContext.h sw/biriscv/freertos/RISC-V_biriscv/portmacro.h sw/biriscv/memcpy.c sw/biriscv/syscalls.c

--- sw/biriscv/baremetal/start.s
+++
@@ -1,7 +0,0 @@
-/* start.S */
-.section .text.start
-.global _start
-
-_start:
-    la sp, _stack_top    # Load the stack pointer address from linker script
-    call biriscv_entry            # Jump to C main function
